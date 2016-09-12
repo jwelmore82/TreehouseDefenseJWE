1607a1749b288e7a703c09dc3a21a7e91b1685f5
Joshua Elmore <jwelmore82@gmail.com>
2016-09-12 15:15:51 -0400

    First commit.

---
+++ TreehouseDefenseJWE/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefenseJWE
+{
+    class Game
+    {
+        static void Main()
+        {
+            Map map = new Map();
+            map.Width = 8;
+            map.Height = 5;
+
+            int area = map.Width * map.Height;
+        }
+    }
+}
